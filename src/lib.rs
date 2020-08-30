ea92a45d218903595b794f892bb8c8788722b523
Lokathor <zefria@gmail.com>
2020-08-30 11:05:16 -0600

    finish the rest of the types.

--- src/lib.rs
+++ src/lib.rs
@@ -268,4 +268,309 @@ bitstruct_newtype! {
   }
 }
 
-// TODO: continue work with the sound types
+bitstruct_newtype! {
+  ToneSweep(u8) {
+    [0-2: sweep_shift_count, set_sweep_shift_count],
+    [3: decreasing_sweep, set_decreasing_sweep],
+    [4-6: sweep_time_chunk, set_sweep_time_chunk],
+  }
+}
+
+const_enum! {
+  ToneWavePatternDuty(u16) {
+    _12_5(0),
+    _25(1),
+    _50(2),
+    _75(3),
+  }
+}
+
+bitstruct_newtype! {
+  ToneDutyLenEnvelope(u16) {
+    [0-5: sound_length, set_sound_length],
+    [6-7 => ToneWavePatternDuty: decreasing_sweep, set_decreasing_sweep],
+    [8-10: envelope_step_time, set_envelope_step_time],
+    [11: increasing_envelope, set_increasing_envelope],
+    [12-15: initial_volume, set_initial_volume],
+  }
+}
+
+bitstruct_newtype! {
+  ToneFrequencyControl(u16) {
+    [0-10: frequency, set_frequency],
+    [14: stop_at_end, set_stop_at_end],
+    [15: init, set_init],
+  }
+}
+
+bitstruct_newtype! {
+  WaveRamSelect(u8) {
+    [5: two_banks, set_two_banks],
+    [6: using_bank1, set_using_bank1],
+    [7: playing, set_playing],
+  }
+}
+
+const_enum! {
+  WaveVolume(u8) {
+    _0(0),
+    _100(1),
+    _50(2),
+    _25(3),
+    _75(0b100),
+  }
+}
+
+bitstruct_newtype! {
+  WaveVolumeSetting(u8) {
+    [5-7 => WaveVolume: volume, set_volume],
+  }
+}
+
+bitstruct_newtype! {
+  WaveFrequencyControl(u16) {
+    [0-10: sample_rate, set_sample_rate],
+    [14: stop_at_end, set_stop_at_end],
+    [15: init, set_init],
+  }
+}
+
+bitstruct_newtype! {
+  NoiseLengthEnvelope(u16) {
+    [0-5: length, set_length],
+    [8-10: envelope_step_time, set_envelope_step_time],
+    [11: increasing_envelope, set_increasing_envelope],
+    [12-15: initial_volume, set_initial_volume],
+  }
+}
+
+bitstruct_newtype! {
+  NoiseFrequencyControl(u16) {
+    /// 0 => 0.5 instead
+    [0-2: dividing_ratio, set_dividing_ratio],
+    [3: step_width_7bits, set_step_width_7bits],
+    [4-7: shift_clock_frequency, set_shift_clock_frequency],
+    [14: stop_at_end, set_stop_at_end],
+    [15: init, set_init],
+  }
+}
+
+bitstruct_newtype! {
+  GeneratedSoundLeftRightMainVolume(u8) {
+    [0-2: right, set_right],
+    [4-6: left, set_left],
+  }
+}
+
+bitstruct_newtype! {
+  GeneratedSoundLeftRightEnabled(u8) {
+    [0: right_sound_1_enabled, set_right_sound_1_enabled],
+    [1: right_sound_2_enabled, set_right_sound_2_enabled],
+    [2: right_sound_3_enabled, set_right_sound_3_enabled],
+    [3: right_sound_4_enabled, set_right_sound_4_enabled],
+    [4: left_sound_1_enabled, set_left_sound_1_enabled],
+    [5: left_sound_2_enabled, set_left_sound_2_enabled],
+    [6: left_sound_3_enabled, set_left_sound_3_enabled],
+    [7: left_sound_4_enabled, set_left_sound_4_enabled],
+  }
+}
+
+const_enum! {
+  GeneratedSoundMixingVolume(u8) {
+    _25(0),
+    _50(1),
+    _100(2),
+  }
+}
+
+bitstruct_newtype! {
+  DmaSoundMixVolumeControl(u8) {
+    [0-1 => GeneratedSoundMixingVolume: generated_volume, set_generated_volume],
+    [2: sound_a_full, set_sound_a_full],
+    [3: sound_b_full, set_sound_b_full],
+  }
+}
+
+bitstruct_newtype! {
+  DmaSoundControlBits(u8) {
+    [0: sound_a_right, set_sound_a_right],
+    [1: sound_a_left, set_sound_a_left],
+    [2: sound_a_timer1, set_sound_a_timer1],
+    [3: sound_a_fifo_reset, set_sound_a_fifo_reset],
+    [4: sound_b_right, set_sound_b_right],
+    [5: sound_b_left, set_sound_b_left],
+    [6: sound_b_timer1, set_sound_b_timer1],
+    [7: sound_b_fifo_reset, set_sound_b_fifo_reset],
+  }
+}
+
+// Note(Lokathor): PSG = Programmable Sound Generator
+
+bitstruct_newtype! {
+  GeneratedSoundActiveBits(u8) {
+    [0: sound_1_active, set_sound_1_active],
+    [1: sound_2_active, set_sound_2_active],
+    [2: sound_3_active, set_sound_3_active],
+    [3: sound_4_active, set_sound_4_active],
+    [7: sound_enabled, set_sound_enabled],
+  }
+}
+
+const_enum! {
+  SoundBiasSamplingSetting(u16) {
+    _9bit(0),
+    _8bit(1),
+    _7bit(2),
+    _6bit(3),
+  }
+}
+
+bitstruct_newtype! {
+  SoundBiasSetting(u16) {
+    [1-9: bias_level, set_bias_level],
+    [14-15 => SoundBiasSamplingSetting: sampling_cycle, set_sampling_cycle],
+  }
+}
+
+const_enum! {
+  TimerScaleFactor(u8) {
+    _1(0),
+    _64(1),
+    _256(2),
+    _1024(3),
+  }
+}
+
+bitstruct_newtype! {
+  TimerControlSetting(u8) {
+    [0-1 => TimerScaleFactor: scale_factor, set_scale_factor],
+    [2: overflow_counting, set_overflow_counting],
+    [6: interrupt_on_overflow, set_interrupt_on_overflow],
+    [7: enabled, set_enabled],
+  }
+}
+
+const_enum! {
+  DmaDestinationAddressControl(u16) {
+    Increment(0),
+    Decrement(1),
+    Fixed(2),
+    IncrementReload(3),
+  }
+}
+
+const_enum! {
+  DmaSourceAddressControl(u16) {
+    Increment(0),
+    Decrement(1),
+    Fixed(2),
+  }
+}
+
+const_enum! {
+  DmaStartTiming(u16) {
+    Immediate(0),
+    Vblank(1),
+    Hblank(2),
+    /// Start time depends on the DMA used.
+    ///
+    /// ## Safety
+    /// * This value is prohibited for DMA0
+    Special(3),
+  }
+}
+
+bitstruct_newtype! {
+  DmaControlSetting(u16) {
+    [5-6 => DmaDestinationAddressControl: dst_addr_control, set_dst_addr_control],
+    [7-8 => DmaSourceAddressControl: src_addr_control, set_src_addr_control],
+    [9: repeating, set_repeating],
+    [10: transfer32, set_transfer32],
+    [12-13: start_timing, set_start_timing],
+    [14: interrupt_when_complete, set_interrupt_when_complete],
+    [15: enabled, set_enabled],
+  }
+}
+
+bitstruct_newtype! {
+  KeyInputLowActive(u16) {
+    [0: a_released, set_a_released],
+    [1: b_released, set_b_released],
+    [2: select_released, set_select_released],
+    [3: start_released, set_start_released],
+    [4: right_released, set_right_released],
+    [5: left_released, set_left_released],
+    [6: up_released, set_up_released],
+    [7: down_released, set_down_released],
+    [8: r_released, set_r_released],
+    [9: l_released, set_l_released],
+  }
+}
+
+bitstruct_newtype! {
+  KeyInterruptBits(u16) {
+    [0: a_selected, set_a_selected],
+    [1: b_selected, set_b_selected],
+    [2: select_selected, set_select_selected],
+    [3: start_selected, set_start_selected],
+    [4: right_selected, set_right_selected],
+    [5: left_selected, set_left_selected],
+    [6: up_selected, set_up_selected],
+    [7: down_selected, set_down_selected],
+    [8: r_selected, set_r_selected],
+    [9: l_selected, set_l_selected],
+    [14: key_interrupts_enabled, set_key_interrupts_enabled],
+    [15: interrupt_requires_all_bits, set_interrupt_requires_all_bits],
+  }
+}
+
+bitstruct_newtype! {
+  InterruptFlagBits(u16) {
+    [0: vblank, set_vblank],
+    [1: hblank, set_hblank],
+    [2: vcount_match, set_vcount_match],
+    [3: timer0, set_timer0],
+    [4: timer1, set_timer1],
+    [5: timer2, set_timer2],
+    [6: timer3, set_timer3],
+    [7: serial_communication, set_serial_communication],
+    [8: dma0, set_dma0],
+    [9: dma1, set_dma1],
+    [10: dma2, set_dma2],
+    [11: dma3, set_dma3],
+    [12: keypad, set_keypad],
+    [13: game_pak, set_game_pak],
+  }
+}
+
+const_enum! {
+  WaitControlCycles(u16) {
+    _4(0),
+    _3(1),
+    _2(2),
+    _8(3),
+  }
+}
+
+const_enum! {
+  PhiTerminalOutput(u16) {
+    Disabled(0),
+    _4_19MHz(1),
+    _8_38MHz(2),
+    _16_78MHz(3),
+  }
+}
+
+bitstruct_newtype! {
+  WaitControlSetting(u16) {
+    [0-1 => WaitControlCycles: sram_wait, set_sram_wait],
+    [2-3 => WaitControlCycles: wait0_first_access, set_wait0_first_access],
+    [4: wait0_second_access_1cycle, set_wait0_second_access_1cycle],
+    [5-6 => WaitControlCycles: wait1_first_access, set_wait1_first_access],
+    [7: wait1_second_access_1cycle, set_wait1_second_access_1cycle],
+    [8-9 => WaitControlCycles: wait2_first_access, set_wait2_first_access],
+    [10: wait2_second_access_1cycle, set_wait2_second_access_1cycle],
+    [11-12 => PhiTerminalOutput: phi_terminal, set_phi_terminal],
+    [14: game_pak_prefetch_enabled, set_game_pak_prefetch_enabled],
+  }
+}
