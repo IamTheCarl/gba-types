77a4ca1c576236c153dce9e8bf310085b76d057d
I am the Carl <jamescarl96@gmail.com>
2020-09-07 15:49:43 -0400

    Spelling mistake corrections.

--- src/timer.rs
+++ src/timer.rs
@@ -4,7 +4,7 @@
 //! The counter registers are simply u16 fields, so we do not bother to create
 //! a special representation of them. Reading from these counter registers will
 //! give you the current count of how many cycles the base frequency has
-//! cycled. Writing to this register will not immediatly change its value, but
+//! cycled. Writing to this register will not immediately change its value, but
 //! rather will set the value to be loaded into it the next time the timer is
 //! enabled/re-enabled.
 
@@ -25,9 +25,9 @@ const_enum! {
 
 bitstruct_newtype! {
   TimerControlSetting(u8) {
-    /// The prescale scale factor that the timer will count at.
+    /// The pre-scale scale factor that the timer will count at.
     [0-1 => TimerScaleFactor: scale_factor, set_scale_factor],
-    /// Instead of incrementing the count with each prescaled clock cycle, will instead increment whenever the lower number timer overflows.
+    /// Instead of incrementing the count with each pre-scaled clock cycle, will instead increment whenever the lower number timer overflows.
     /// Timer0 cannot make effective use of this feature because there is no lower number timer than it.
     [2: overflow_counting, set_overflow_counting],
     /// Set to 1 to generate an interrupt when this timer overflows.
