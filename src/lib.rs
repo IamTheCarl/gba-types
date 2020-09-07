539774f1ed8ef0196eeb878998a33835886f73a6
I am the Carl <jamescarl96@gmail.com>
2020-09-06 22:45:47 -0400

    Documented DMA.

--- src/lib.rs
+++ src/lib.rs
@@ -571,45 +571,88 @@ bitstruct_newtype! {
   }
 }
 
-const_enum! {
-  DmaDestinationAddressControl(u16) {
-    Increment(0 << 5),
-    Decrement(1 << 5),
-    Fixed(2 << 5),
-    IncrementReload(3 << 5),
+/// DMA on the GBA is very different from most embedded devices. It does not run in parallel to the CPU, but rather
+/// stops the CPU to preform the memory transfer. Dispite the fact that it stops the CPU, it is still generally faster
+/// than having the CPU copy data by itself.
+/// 
+/// There are 4 DMA channels, DMA0, DMA1, DMA2, and DMA3.
+/// 
+/// DMA0 is the highest priority channel and will always complete its job before any other channel. This is ideal for
+/// time critical operations, such as copying data to a horizontal scanline. It has the restriction of only being able
+/// to access internal memory, so it cannot access the game pak.
+/// 
+/// DMA1 and DMA2 are intended to be used for feeding sound data into the audio FIFOs.
+/// 
+/// DMA3 is special because it is capable of writing to game pak ROM/FlashROM, but is unable to write to game pak SRAM.
+/// 
+/// However it should be noted that these are just the intended use of the DMAs. You can use them for other purposes
+/// as you see fit.
+mod dma {
+  const_enum! {
+    /// Destination control settings.
+    DmaDestinationAddressControl(u16) {
+      /// Increment the address with each copy.
+      Increment(0 << 5),
+      /// Decrement the address with each copy.
+      Decrement(1 << 5),
+      /// Do not move.
+      Fixed(2 << 5),
+      /// TODO working on figuring out what makes this special.
+      IncrementReload(3 << 5),
+    }
   }
-}
-
-const_enum! {
-  DmaSourceAddressControl(u16) {
-    Increment(0),
-    Decrement(1),
-    Fixed(2),
+  
+  const_enum! {
+    /// Source control settings.
+    DmaSourceAddressControl(u16) {
+      /// Increment the address with each copy.
+      Increment(0),
+      /// Decrement the address with each copy.
+      Decrement(1),
+      /// Do not move.
+      Fixed(2),
+    }
   }
-}
-
-const_enum! {
-  DmaStartTiming(u16) {
-    Immediate(0),
-    Vblank(1),
-    Hblank(2),
-    /// Start time depends on the DMA used.
-    ///
-    /// ## Safety
-    /// * This value is prohibited for DMA0
-    Special(3),
+  
+  const_enum! {
+    /// Which event to trigger the DMA on.
+    DmaStartTiming(u16) {
+      /// Starts the DMA as soon as you set enabled to true.
+      Immediate(0),
+      /// Start the DMA on a vblank interrupt.
+      Vblank(1),
+      /// Start the DMA on an hblank interrupt.
+      Hblank(2),
+      /// Start time depends on the DMA used.
+      ///
+      /// DMA0: prohibited. Do not use.
+      /// DMA1/2: Sound FIFO
+      /// DMA3: Video Capture
+      /// ## Safety
+      /// * This value is prohibited for DMA0
+      Special(3),
+    }
   }
-}
-
-bitstruct_newtype! {
-  DmaControlSetting(u16) {
-    [5-6 => DmaDestinationAddressControl: dst_addr_control, set_dst_addr_control],
-    [7-8 => DmaSourceAddressControl: src_addr_control, set_src_addr_control],
-    [9: repeating, set_repeating],
-    [10: transfer32, set_transfer32],
-    [12-13 => DmaStartTiming: start_timing, set_start_timing],
-    [14: interrupt_when_complete, set_interrupt_when_complete],
-    [15: enabled, set_enabled],
+  
+  bitstruct_newtype! {
+    /// use to control a DMA channel.
+    DmaControlSetting(u16) {
+      /// Settings for how to treat the destination address.
+      [5-6 => DmaDestinationAddressControl: dst_addr_control, set_dst_addr_control],
+      /// Settings for how to treat the source address.
+      [7-8 => DmaSourceAddressControl: src_addr_control, set_src_addr_control],
+      /// If cleared to 0, then the enabled bit (15) will be cleared as well when DMA is complete.
+      /// If set to 1, then the enable bit will remain set and the DMA will repeat when its start event happens again.
+      [9: repeating, set_repeating],
+      /// Set to 1 to preform a transfer at 32bits at a time. Clear to 0 to transfer 16bits at a time.
+      [10: transfer32, set_transfer32],
+      /// Set the event to trigger the DMA.
+      [12-13 => DmaStartTiming: start_timing, set_start_timing],
+      /// Set to 1 to trigger an interrupt when complete.
+      [14: interrupt_when_complete, set_interrupt_when_complete],
+      /// Set to 1 to enable.
+      [15: enabled, set_enabled],
+    }
   }
 }
 
